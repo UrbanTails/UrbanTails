b01b15c0f59a660e90cae30fadefd88d696f6a87
qsmith1 <smith.queenie@gmail.com>
2018-02-21 19:05:43 -0500

    Connect database to server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const db = require('../database/index');
 let app = express();
 
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
