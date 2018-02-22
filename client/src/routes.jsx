2ada059fe72dc67a02a437b632cc4afc0ddbf222
michael shin <michael.shin.hrr@gmail.com>
2018-02-21 16:21:12 -0800

    rebase with database

---
+++ client/src/routes.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from './components/app.jsx';
+import Login from './components/login.jsx';
+import Signup from './components/signup.jsx';
