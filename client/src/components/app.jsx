50b3f597986d8c10bc5b3481c41499b0ab917d8f
qsmith1 <smith.queenie@gmail.com>
2018-02-25 14:49:33 -0500

    Modify router and routes

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import { Route, IndexRoute, hashHistory, browserHistory, Router } from 'react-router';
-import { HashRouter } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
+import Landing from './landing.jsx';
 import Login from './login.jsx';
 import Signup from './signup.jsx';
-import Landing from './landing.jsx';
-
+import PetProfile from './pet-owner-profile.jsx';
 
 class App extends React.Component {
   render () {
     return (
       <div>
-        <Route exact path='/' component={Landing} />
-        <Route path='/login' component={Login} />
-        <Route path='/signup' component={Signup} />
+        <Route exact path='/' component={ Landing } />
+        <Route path='/signup' component={ Signup } />
+        <Route path='/login' component={ Login } />
+        <Route path='/pet-profile' component={ PetProfile } />
       </div>
     )
   }
-
-  
 }
 
 module.exports = App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, browserHistory } from 'react-router-dom';
 import App from './components/app.jsx'
 
-import { BrowserRouter } from 'react-router-dom';
-
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('app'));
+ReactDOM.render(
+  <BrowserRouter history={ browserHistory }>
+    <App />
+  </BrowserRouter>, document.getElementById('app'));
