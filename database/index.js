551327f5a9250bc5531c5e08c23f1b500d16fdd1
Annah Patterson Pelin <annah.pelin@gmail.com>
2018-02-27 18:11:07 -0500

    feat/hashedpassword-#49

--- database/index.js
+++ database/index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const saltRounds = 5;
+let Schema = mongoose.Schema;
 let uristring = process.env.MONGODB_URI || 'mongodb://localhost:27017/users';
 
 mongoose.connect(uristring, (err) => {
@@ -12,13 +14,19 @@ mongoose.connect(uristring, (err) => {
 let PetOwnerSchema = new Schema({
   username: String,
   password: String,
-  type: String
+  profileUrl: String,
+  type: String,
+  location: String,
+  description: String
 });
 
 let HostSchema = new Schema({
   username: String,
   password: String,
-  type: String
+  profileUrl: String,
+  type: String,
+  location: String,
+  description: String
 });
 
 let PetOwner = mongoose.model('PetOwner', PetOwnerSchema);
@@ -26,27 +34,52 @@ let Host = mongoose.model('Host', HostSchema);
 
 
 module.exports = {
+  getUser: () => {
+
+  },
   saveUser: (data, callback) => {
-    let petOwner = new PetOwner ({
+    let pass = data.password;
+
+    bcrypt.hash(pass, saltRounds, (err, hash) => {
+      let petOwner = new PetOwner ({
       username: data.username,
-      password: data.password,
-      type: 'petOwner'
+      password: hash,
+      profileUrl: data.profileUrl,
+      type: 'petOwner',
+      location: data.location,
+      description: data.description
+      });
+
+
+      petOwner.save((err, petOwner) => {
+        callback(err, petOwner);
+      });
     });
 
-    petOwner.save((err, petOwner) => {
-      callback(err, petOwner);
-    });
+  },
+  getHost: () => {
+
   },
   saveHost: (data, callback) => {
-    let host = new Host ({
+    let pass = data.password;
+
+    bcrypt.hash(pass, saltRounds, (err, hash) => {
+      let host = new Host ({
       username: data.username,
-      password: data.password,
-      type: 'host'
+      password: hash,
+      profileUrl: data.profileUrl,
+      type: 'host',
+      location: data.location,
+      description: data.description
+      });
+
+
+      host.save((err, host) => {
+        callback(err, host);
+      });
     });
 
-    host.save((err, host) => {
-      callback(err, host);
-    });
+
   },
   dropDatabase: () => {
     mongoose.connection.dropDatabase();
