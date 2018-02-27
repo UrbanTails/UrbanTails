61903f070539b66582c51acf21dcafbd8513e9f8
Annah Patterson Pelin <annah.pelin@gmail.com>
2018-02-27 15:17:44 -0500

    Seed database with data #41

--- seed.js
+++ seed.js
@@ -4,7 +4,6 @@ let sampleHosts = require('./sampleHosts.json');
 
 db.dropDatabase();
 
-
 samplePetOwners.forEach((obj, err) => {
   db.saveUser(obj, (err, data) => {
     if (err) { console.log('error saving pet owner to database', err); }
