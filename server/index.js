0368c55663496131fabc1f0dc595d6080d903ed2
Yufan Wang <yufansmail@gmail.com>
2018-02-28 15:26:04 -0800

    added route to check users before sign up

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const session = require('express-session');
 
-
 const bodyParser = require('body-parser');
 const db = require('../database/index');
 let app = express();
@@ -21,11 +20,10 @@ app.use(session({
 }));
 
 app.route('/login')
-  .post((req, res) => {
+   .post((req, res) => {
     console.log('getting user from database:', req.body);
 
     db.getUser(req.body, (err, result) => {
-      // if (err) { console.log('error getting userdata from db:', err); }
       if (err) { res.status(500).send(err); }
       else {
         console.log('got user data from the db:', result);
@@ -34,36 +32,37 @@ app.route('/login')
     });
 });
 
-app.route('/users')
-  .get((req, res) => {
-    res.send('user GET');
-  })
-  .post((req, res) => {
-    res.send('user POST');
+app.route('/checkuser')
+   .post((req, res) => {
+
+    db.getUser(req.body, function(err, result) {
+      if (err) {
+        res.send({ 'exists': false });
+      } else {
+        res.send({ 'exists': true });
+      }
+    });
 });
 
 app.route('/signup')
-  .post((req, res) => {
-  console.log('posting new user to db:', req.body);
+   .post((req, res) => {
+    console.log('posting new user to db:', req.body);
 
-  db.saveUser(req.body, (err, result) => {
-    if (err) {
-      console.log('error saving user data to db:', err);
-      res.status(500).send({ error: 'User already exists' });
-    }
-    else {
-      console.log('saved user data to the db:', result);
-      res.send(result);
-    }
-  });
+    db.saveUser(req.body, (err, result) => {
+      if (err) {
+        console.log('error saving user data to db:', err);
+        res.status(500).send({ error: 'User already exists' });
+      }
+      else {
+        console.log('saved user data to the db:', result);
+        res.send(result);
+      }
+    });
 });
 
-app.route('/messages', (req, res) => {
-
-});
 
 app.route('/*')
-  .get((req, res) => {
+   .get((req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
