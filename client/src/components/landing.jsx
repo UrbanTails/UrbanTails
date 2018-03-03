1924b9796a91563b90668475bc0dd6ba6a3f1f31
qsmith1 <smith.queenie@gmail.com>
2018-03-03 15:57:16 -0500

    Add avatars

--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -5,7 +5,6 @@ import { Avatar } from 'material-ui';
 import Navbar from './navbar.jsx';
 import Footer from './footer.jsx';
 
-
 class Landing extends React.Component {
   render() {
     return (
@@ -18,27 +17,27 @@ class Landing extends React.Component {
             <Link to='/signup' className="btn btn-default btn-lg">Get Started</Link>
           </div>
         </Jumbotron>
-        <div class="container">
+        <div className="container">
           <h2 className="heading-landing">How it works</h2>
           <Row className="landing-content">
             <Col md={3}>
-              <i class="material-icons md-36">face</i>
+              <i className="material-icons md-36">face</i>
               <h3>Start</h3>
               <p>Create a free profile as either a pet owner or a host for pets</p>
             </Col>
             <Col md={3}>
-              <i class="material-icons md-36">autorenew</i>
+              <i className="material-icons md-36">autorenew</i>
               <h3>Connect</h3>
               <p>Searching for places to stay or open your home to host pets and their owners.</p>
             </Col>
             <Col md={3}>
-              <i class="material-icons md-36">today</i>
+              <i className="material-icons md-36">today</i>
               <h3>Plan</h3>
 
               <p>Choose a listing and plan your stay or manage your bookings.</p>
             </Col>
             <Col md={3}>
-              <i class="material-icons md-36">thumb_up</i>
+              <i className="material-icons md-36">thumb_up</i>
               <h3>Share</h3>
               <p>Share your experience and help build a community of animal lovers.</p>
             </Col>
@@ -47,22 +46,22 @@ class Landing extends React.Component {
           <h2 className="heading-landing">About Us</h2>
           <Row className="landing-content padded">
             <Col md={3} className="landing-box">
-              <Avatar src="" size={60}/>
+              <Avatar src="https://avatars3.githubusercontent.com/u/22921336?s=460&v=4" size={100}/>
               <h4>Annah Patterson</h4>
               <p>Lead Engineer</p>
             </Col>
             <Col md={3}>
-              <Avatar src="" size={60}/>
+              <Avatar src="" size={100}/>
               <h4>Michael Shin</h4>
               <p>Scrum Master</p>
             </Col>
             <Col md={3}>
-              <Avatar src="" size={60}/>
+              <Avatar src="https://avatars0.githubusercontent.com/u/28540710?s=460&v=4" size={100}/>
               <h4>Queenie Smith</h4>
               <p>Product Owner</p>
             </Col>
             <Col md={3}>
-              <Avatar src="" size={60}/>
+              <Avatar src="https://avatars2.githubusercontent.com/u/31011353?s=400&v=4" size={100}/>
               <h4>Yufan Wang</h4>
               <p>Lead Engineer</p>
             </Col>
