2c2f3f00e88cc89d34a95fb9de9dcc260e4de01e
Joe Ford <joefordiv7@gmail.com>
2018-03-09 18:27:15 -0500

    stripe up and working

--- client/src/components/singleListView.jsx
+++ client/src/components/singleListView.jsx
@@ -3,6 +3,7 @@ import { Row, Col } from 'react-bootstrap';
 import Navbar from './navbar.jsx';
 import Searchbar from './searchbar.jsx';
 import Footer from './footer.jsx';
+import Checkout from './checkout.jsx'
 /*
   Single Listing View:
   Used by login redirect and by clicking 'My Account' on listings page
@@ -28,13 +29,18 @@ class ListingProfile extends React.Component {
         <Navbar user={this.state.user} search={true}/>
         <h1>Your Pet's adventure awaits.</h1>
         <h3>Location: {this.state.location}</h3>
-        <p><h3>Description: </h3></p>
-        <p className="description"><h4>{this.state.description}</h4></p>
+        <h3><p>Description: </p></h3>
+        <h4><p className="description">{this.state.description}</p></h4>
         <div className="card">
           <img className="card-img-top" src={this.state.profileUrl} alt="Card image cap" />
           <div className="card-block">
             <h4 className="card-title">{this.state.hostName}</h4>
             <p><b>Email: </b>{this.state.email}</p>
+            <Checkout
+            name={'Your Freedom Awaits'}
+            description={'Ask about becoming a host'}
+            amount={1}
+            />
             <p className="card-text">We only accept Cash</p>
             <a href="#" className="btn-lg btn-primary">Book</a>
           </div>
