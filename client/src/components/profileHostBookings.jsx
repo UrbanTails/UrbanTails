a04a63c5478c0a5c43fbb44b6dc09525c5445538
John Blaine <jablaine@uw.edu>
2018-03-10 13:11:45 -0800

    Create host booking listings

--- client/src/components/profileHostBookings.jsx
+++ client/src/components/profileHostBookings.jsx
@@ -19,9 +19,9 @@ var ProfileHostBookings = (props) => {
       <div>
         <h3 style={style}>Bookings {props.user} is hosting:</h3>
         <List>
-          this.props.hostBookings.map((booking, index) => {
-          <ListItem primaryText={"Booking" + index}/>
-        });
+        {props.ownerBookings.map((booking, index) =>
+          <ListItem leftAvatar={<Avatar src={booking.profileUrl}/>} primaryText={"Booking for" + booking.userName} secondaryText={"Start Date: " + booking.startDate + " / " + "End Date: " + booking.endDate}/>
+        )}
         </List>
       </div>
     )
