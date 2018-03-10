121929cb66785dc62bac65dd28dbd511e194cfc9
gkolb <gkolb100@gmail.com>
2018-03-10 00:17:43 -0700

    Fixed bug that caused page to rerender when Brand is clicked

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -27,19 +27,19 @@ class Navbar extends React.Component {
         <nav className="navbar navbar-light">
           <div className="container-fluid top-nav">
             <div className="navbar-header">
-              <a className="navbar-brand" href="/">
+              <Link className="navbar-brand" to={{ pathname: '/', state: this.state.user }}>
                 <i className="material-icons">pets</i>Urban Tails
-              </a>
+              </Link>
             </div>
-
+            <div>{ this.props.search ? <Searchbar setresults={this.state.setresults}/> : null }</div>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="nav navbar-nav navbar-right mr-auto">
+              <ul className="nav navbar-nav navbar-right">
 
-            <li className="nav-item active"><a href="#">Become a Host</a></li>
-            <li className="nav-item active"><a href="#">Help</a></li>
-            <li className="nav-item active"><a href="#">About Us</a></li>
-              <li className="nav-item active"><Link to={{ pathname: this.state.linkurl, state: this.state.user }}>{this.state.link}</Link></li>
-            </ul>
+                <li className="nav-item active"><a href="#">Become a Host</a></li>
+                <li className="nav-item active"><a href="#">Help</a></li>
+                <li className="nav-item active"><a href="#">About Us</a></li>
+                <li className="nav-item active"><Link to={{ pathname: this.state.linkurl, state: this.state.user }}>{this.state.link}</Link></li>
+              </ul>
             </div>
           </div>
         </nav>
