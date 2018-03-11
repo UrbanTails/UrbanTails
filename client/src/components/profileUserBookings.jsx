3c3beab99d6844b2ee8d7057089d860ed6315389
John Blaine <jablaine@uw.edu>
2018-03-11 09:50:07 -0700

    Slight re-styling of host profile

--- client/src/components/profileUserBookings.jsx
+++ client/src/components/profileUserBookings.jsx
@@ -11,14 +11,14 @@ var ProfileUserBookings = (props) => {
   if (props.userBookings.length === 0) {
     return(
       <div>
-        <h3 style={style}>{"Bookings for " + props.user + "'s pet:"}:</h3>
+        <h3 style={style}>{"Bookings for " + props.user + "'s pet:"}</h3>
         <div style={style}>No bookings yet.</div>
       </div>
     );
   } else {
     return (
       <div>
-        <h3 style={style}>{"Bookings for " + props.user + "'s pet:"}:</h3>
+        <h3 style={style}>{"Bookings for " + props.user + "'s pet:"}</h3>
         <List>
         {props.userBookings.map((booking, index) =>
           <ListItem primaryText={"Booking for " + booking.userName} secondaryText={"Start Date: " + booking.startDate + " / " + "End Date: " + booking.enddate}/>
